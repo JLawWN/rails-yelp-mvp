be762514c108565dc5f3db6354692cf093e843d7
Law WN <lawwnc@gmail.com>
2018-05-23 14:01:20 +0900

    routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-restaurants = Restaurant.create([{name: "Toast Box", address: "Yew Tee Point"},
-  {name: "Song Fa", address: "Bukit Panjang Plaza"},
-  {name: "A1", address: "Hillion Mall"},
-  {name: "Al Amaan", address: "Clementi"},
-  {name: "Habitat", address: "Upper Thompson"} ])
+restaurants_attributes = [{name: "Toast Box", address: "Yew Tee Point", category: "chinese"},
+  {name: "Acqua e Farina", address: "Rail Mall", category: "italian"},
+  {name: "MOF", address: "Hillion Mall", category: "japanese"},
+  {name: "Gunther's", address: "Purvis Street", category: "french"},
+  {name: "Habitat", address: "Upper Thompson", category: "belgian"} ]
+Restaurant.create!(restaurants_attributes)
