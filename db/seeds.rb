7dda61de67315b8fcf3f6715f0c4a9187f23f43e
Law WN <lawwnc@gmail.com>
2018-05-23 12:25:58 +0900

    models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurants = Restaurant.create([{name: "Toast Box", address: "Yew Tee Point"},
+  {name: "Song Fa", address: "Bukit Panjang Plaza"},
+  {name: "A1", address: "Hillion Mall"},
+  {name: "Al Amaan", address: "Clementi"},
+  {name: "Habitat", address: "Upper Thompson"} ])
