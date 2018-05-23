e1cb9cda671b3a595fdd551248d6dc770c6fa7ae
Law WN <lawwnc@gmail.com>
2018-05-23 14:12:18 +0900

    jquery bundle

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  # get 'reviews/new'
+  # get 'reviews/create'
+  # get 'restaurants/index'
+  # get 'restaurants/new'
+  # get 'restaurants/create'
+  # get 'restaurants/show'
+  # get 'restaurant/index'
+  # get 'restaurant/new'
+  # get 'restaurant/create'
+  # get 'restaurant/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get "restaurants", to: "restaurants#index"
   # get "restaurants/new", to: "restaurants#new"
